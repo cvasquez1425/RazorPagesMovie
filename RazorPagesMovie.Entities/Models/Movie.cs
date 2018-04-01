c00164473c07fc39ad66a6d20ab2291a59dfe9a5
Carlos B. Vasquez <cvasquez1425@hotmail.com>
2018-04-01 13:35:54 -0400

    First Commit

---
+++ RazorPagesMovie.Entities/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesMovie.Entities.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
